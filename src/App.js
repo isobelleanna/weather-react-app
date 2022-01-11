a70653c4afb84bbb7f5221ac058b453efea27ca0
isobelleanna <67749649+isobelleanna@users.noreply.github.com>
2022-01-11 18:55:19 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import "./SearchEngine";
+import SearchEngine from "./SearchEngine";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Weather Search Engine</h1>
+      <SearchEngine />
+    </div>
+  );
+}
+
+
